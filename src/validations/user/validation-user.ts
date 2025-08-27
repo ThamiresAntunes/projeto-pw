99a8fe7900e7674a6104cc9054cc947b953f5272
ThamiresAntunes <antunesthamires8@gmail.com>
2025-08-27 11:18:46 -0300

    criando validacao de campos para update de login de user e modificações de controller

--- src/validations/user/validation-user.ts
+++ src/validations/user/validation-user.ts
@@ -6,10 +6,14 @@ export const userSchema = z.object({
     .min(3, "O nome deve ter pelo menos 3 caracteres")
     .max(50, "O nome pode ter no máximo 50 caracteres"),
 
-  email: z.string().email("E-mail inválido"),
+  email: z.string().email({ message: "E-mail inválido" }),
 
   password: z.string()
     .min(6, "A senha deve ter pelo menos 6 caracteres")
     .max(20, "A senha pode ter no máximo 100 caracteres"),
 });
+
+
+//Schema para validar formatação dos dados para a atualização do login de user
+export const userUpdateSchema = userSchema.partial();
 
